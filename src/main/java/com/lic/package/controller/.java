b8866e1e7145d25d220c93057ab57ed025c6b377
licproject <143190336+licproject@users.noreply.github.com>
2023-09-15 14:37:38 +0530

    initial commit

--- src/main/java/com/lic/package/controller/.java
+++ src/main/java/com/lic/package/controller/.java
@@ -1,1 +1,51 @@
-Solution: Saving Member Details:INSERT INTO POLICY_SRV_MBR (MEMBER_ID, AADHAR_NUMBER, CATEGORY_NO, COMMUNICATION_PREFERENCE, CREATED_BY, CREATED_ON, DATE_OF_BIRTH, DATE_OF_JOINING, DATE_OF_JOINING_SCHEME, DATE_OF_RETIREMENT, DESIGNATION, EMAILID, FATHER_NAME, FIRST_NAME, GENDER, IS_ACTIVE, IS_ZEROID, LANDLINE_NO, LANGUAGE_PREFERENCE, LAST_NAME, LIC_ID, MARITAL_STATUS, MEMBER_PAN, MEMBER_STATUS, MEMBERSHIP_NUMBER, MIDDLE_NAME, MOBILE_NO, MODIFIED_BY, MODIFIED_ON, POLICY_IDS, SPOUSE_NAME, TYPE_OF_MEMBERSHIP)VALUES (:memberId, :aadharNumber, :categoryNo, :communicationPreference, :createdBy, :createdOn, :dateOfBirth, :dateOfJoining, :dateOfJoiningScheme, :dateOfRetirement, :designation, :emailId, :fatherName, :firstName, :gender, :isActive, :isZeroId, :landlineNo, :languagePreference, :lastName, :licId, :maritalStatus, :memberPan, :memberStatus, :membershipNumber, :middleName, :mobileNo, :modifiedBy, :modifiedOn, :policyIds, :spouseName, :typeOfMembership);Updating Member Details:UPDATE POLICY_SRV_MBR SET AADHAR_NUMBER = :aadharNumber, CATEGORY_NO = :categoryNo, COMMUNICATION_PREFERENCE = :communicationPreference, CREATED_BY = :createdBy, CREATED_ON = :createdOn, DATE_OF_BIRTH = :dateOfBirth, DATE_OF_JOINING = :dateOfJoining, DATE_OF_JOINING_SCHEME = :dateOfJoiningScheme, DATE_OF_RETIREMENT = :dateOfRetirement, DESIGNATION = :designation, EMAILID = :emailId, FATHER_NAME = :fatherName, FIRST_NAME = :firstName, GENDER = :gender, IS_ACTIVE = :isActive, IS_ZEROID = :isZeroId, LANDLINE_NO = :landlineNo, LANGUAGE_PREFERENCE = :languagePreference, LAST_NAME = :lastName, LIC_ID = :licId, MARITAL_STATUS = :maritalStatus, MEMBER_PAN = :memberPan, MEMBER_STATUS = :memberStatus, MEMBERSHIP_NUMBER = :membershipNumber, MIDDLE_NAME = :middleName, MOBILE_NO = :mobileNo, MODIFIED_BY = :modifiedBy, MODIFIED_ON = :modifiedOn, POLICY_IDS = :policyIds, SPOUSE_NAME = :spouseName, TYPE_OF_MEMBERSHIP = :typeOfMembershipWHERE MEMBER_ID = :memberId;Saving Member Address:INSERT INTO POLICY_SRV_MBR_ADD (MEMBER_ADDITION_ID, CREATED_BY, CREATED_ON, EMPLOYEE_CONTRIBUTION, EMPLOYER_CONTRIBUTION, IS_ACTIVE, LINE_OF_BUSINESS, MEMBER_ADDITION_STATUS, MODIFIED_BY, MODIFIED_ON, MPH_CODE, MPH_NAME, POLICY_ID, POLICY_MBR_NO, POLICY_NUMBER, POLICY_STATUS, PRODUCT, REJECTION_REASON_CODE, REJECTION_REMARKS, SERVICE_ID, SERVICE_NUMBER, SERVICE_STATUS, TOTAL_CONTRIBUTION, UNIT_CODE, VOLUNTARY_CONTRIBUTION, NO_OF_CATEGORY) VALUES (:memberAdditionId, :createdBy, :createdOn, :employeeContribution, :employerContribution, :isActive, :lineOfBusiness, :memberAdditionStatus, :modifiedBy, :modifiedOn, :mphCode, :mphName, :policyId, :policyMbrNo, :policyNumber, :policyStatus, :product,
+MBR_ADD> findByPOLICY_ID(Long POLICY_ID) {
+        return memberAddressRepository.findByPOLICY_ID(POLICY_ID);
+    }
+
+    public List<POLICY_SRV_MBR_ADD> findByPOLICY_NUMBER(String POLICY_NUMBER) {
+        return memberAddressRepository.findByPOLICY_NUMBER(POLICY_NUMBER);
+    }
+
+    public List<POLICY_SRV_MBR_ADD> findByPOLICY_STATUS(String POLICY_STATUS) {
+        return memberAddressRepository.findByPOLICY_STATUS(POLICY_STATUS);
+    }
+
+    public List<POLICY_SRV_MBR_ADD> findByPRODUCT(String PRODUCT) {
+        return memberAddressRepository.findByPRODUCT(PRODUCT);
+    }
+
+    public List<POLICY_SRV_MBR_ADD> findByREJECTION_REASON_CODE(String REJECTION_REASON_CODE) {
+        return memberAddressRepository.findByREJECTION_REASON_CODE(REJECTION_REASON_CODE);
+    }
+
+    public List<POLICY_SRV_MBR_ADD> findBySERVICE_ID(Long SERVICE_ID) {
+        return memberAddressRepository.findBySERVICE_ID(SERVICE_ID);
+    }
+
+    public List<POLICY_SRV_MBR_ADD> findBySERVICE_NUMBER(String SERVICE_NUMBER) {
+        return memberAddressRepository.findBySERVICE_NUMBER(SERVICE_NUMBER);
+    }
+
+    public List<POLICY_SRV_MBR_ADD> findBySERVICE_STATUS(String SERVICE_STATUS) {
+        return memberAddressRepository.findBySERVICE_STATUS(SERVICE_STATUS);
+    }
+
+    public List<POLICY_SRV_MBR_ADD> findByTOTAL_CONTRIBUTION(String TOTAL_CONTRIBUTION) {
+        return memberAddressRepository.findByTOTAL_CONTRIBUTION(TOTAL_CONTRIBUTION);
+    }
+
+    public List<POLICY_SRV_MBR_ADD> findByUNIT_CODE(String UNIT_CODE) {
+        return memberAddressRepository.findByUNIT_CODE(UNIT_CODE);
+    }
+
+    public List<POLICY_SRV_MBR_ADD> findByVOLUNTARY_CONTRIBUTION(String VOLUNTARY_CONTRIBUTION) {
+        return memberAddressRepository.findByVOLUNTARY_CONTRIBUTION(VOLUNTARY_CONTRIBUTION);
+    }
+
+    public List<POLICY_SRV_MBR_ADD> findByNO_OF_CATEGORY(String NO_OF_CATEGORY) {
+        return memberAddressRepository.findByNO_OF_CATEGORY(NO_OF_CATEGORY);
+    }
+
+    @Transactional
+    public void updateMemberAddress(Long MEMBER_ADDITION_ID, String CREATED_BY, String EMPLOYEE_CONTRIBUTION, String EMPLOYER_CONTRIBUTION, String IS_ACTIVE, String LINE_OF_BUSINESS, String MEMBER_ADDITION_STATUS, String MODIFIED_BY, String MPH_CODE, String MPH_NAME, Long POLICY_ID, String POLICY_MBR_NO, String POLICY_NUMBER, String POLICY_STATUS, String PRODUCT, String REJECTION_REASON_CODE, String REJECTION_REMARKS, Long SERVICE_ID, String SERVICE_NUMBER, String SERVICE_STATUS, String TOTAL_CONTRIBUTION, String UNIT_CODE, String VOLUNTARY_CONTRIBUTION, String NO_OF_CATEGORY) {
+        memberAddressRepository.updateMemberAddress(MEMBER_ADDITION_ID, CREATED_BY, EMPLOYEE_CONTRIBUTION, EMPLOYER_CONTRIBUTION, IS_ACTIVE, LINE_OF_BUSINESS, MEMBER_ADDITION_STATUS, MODIFIED_BY, MPH_CODE, MPH_NAME, POLICY_ID, POLICY_MBR_NO, POLICY_
--- src/main/java/com/lic/package/model/POLICY_SRV_MBR_ADD.java
+++ src/main/java/com/lic/package/model/POLICY_SRV_MBR_ADD.java
@@ -1,57 +1,61 @@
-public class POLICY_SRV_MBR_ADD {
-
-    private Long memberAdditionId;
-
-    private String createdBy;
-
-    private Timestamp createdOn;
-
-    private Integer employeeContribution;
-
-    private Integer employerContribution;
-
-    private String isActive;
-
-    private String lineOfBusiness;
-
-    private String memberAdditionStatus;
-
-    private String modifiedBy;
-
-    private Timestamp modifiedOn;
-
-    private String mphCode;
-
-    private String mphName;
-
-    private Long policyId;
-
-    private String policyMbrNo;
-
-    private String policyNumber;
-
-    private String policyStatus;
-
-    private String product;
-
-    private String rejectionReasonCode;
-
-    private String rejectionRemarks;
-
-    private Long serviceId;
-
-    private String serviceNumber;
-
-    private String serviceStatus;
-
-    private Integer totalContribution;
-
-    private String unitCode;
-
-    private Integer voluntaryContribution;
-
-    private Integer noOfCategory;
-
-    // Getters and Setters for all the above fields
+VICE_NUMBER, SERVICE_STATUS = :SERVICE_STATUS, TOTAL_CONTRIBUTION = :TOTAL_CONTRIBUTION, UNIT_CODE = :UNIT_CODE, VOLUNTARY_CONTRIBUTION = :VOLUNTARY_CONTRIBUTION, NO_OF_CATEGORY = :NO_OF_CATEGORY WHERE MEMBER_ADDITION_ID = :MEMBER_ADDITION_ID", nativeQuery = true)
+    int updateMemberAddress(@Param("MEMBER_ADDITION_ID") Long MEMBER_ADDITION_ID,@Param("CREATED_BY") String CREATED_BY, @Param("EMPLOYEE_CONTRIBUTION") String EMPLOYEE_CONTRIBUTION, @Param("EMPLOYER_CONTRIBUTION") String EMPLOYER_CONTRIBUTION, @Param("IS_ACTIVE") String IS_ACTIVE, @Param("LINE_OF_BUSINESS") String LINE_OF_BUSINESS, @Param("MEMBER_ADDITION_STATUS") String MEMBER_ADDITION_STATUS, @Param("MODIFIED_BY") String MODIFIED_BY, @Param("MPH_CODE") String MPH_CODE, @Param("MPH_NAME") String MPH_NAME, @Param("POLICY_ID") Long POLICY_ID, @Param("POLICY_MBR_NO") String POLICY_MBR_NO, @Param("POLICY_NUMBER") String POLICY_NUMBER, @Param("POLICY_STATUS") String POLICY_STATUS, @Param("PRODUCT") String PRODUCT, @Param("REJECTION_REASON_CODE") String REJECTION_REASON_CODE, @Param("REJECTION_REMARKS") String REJECTION_REMARKS, @Param("SERVICE_ID") Long SERVICE_ID, @Param("SERVICE_NUMBER") String SERVICE_NUMBER, @Param("SERVICE_STATUS") String SERVICE_STATUS, @Param("TOTAL_CONTRIBUTION") String TOTAL_CONTRIBUTION, @Param("UNIT_CODE") String UNIT_CODE, @Param("VOLUNTARY_CONTRIBUTION") String VOLUNTARY_CONTRIBUTION, @Param("NO_OF_CATEGORY") String NO_OF_CATEGORY);
 
 }
+
+@Entity
+@Table(name = "POLICY_SRV_MBR_ADD")
+@Data
+public class POLICY_SRV_MBR_ADD {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ADDITION_ID")
+    private Long MEMBER_ADDITION_ID;
+
+    @Column(name = "CREATED_BY")
+    private String CREATED_BY;
+
+    @Column(name = "EMPLOYEE_CONTRIBUTION")
+    private String EMPLOYEE_CONTRIBUTION;
+
+    @Column(name = "EMPLOYER_CONTRIBUTION")
+    private String EMPLOYER_CONTRIBUTION;
+
+    @Column(name = "IS_ACTIVE")
+    private String IS_ACTIVE;
+
+    @Column(name = "LINE_OF_BUSINESS")
+    private String LINE_OF_BUSINESS;
+
+    @Column(name = "MEMBER_ADDITION_STATUS")
+    private String MEMBER_ADDITION_STATUS;
+
+    @Column(name = "MODIFIED_BY")
+    private String MODIFIED_BY;
+
+    @Column(name = "MPH_CODE")
+    private String MPH_CODE;
+
+    @Column(name = "MPH_NAME")
+    private String MPH_NAME;
+
+    @Column(name = "POLICY_ID")
+    private Long POLICY_ID;
+
+    @Column(name = "POLICY_MBR_NO")
+    private String POLICY_MBR_NO;
+
+    @Column(name = "POLICY_NUMBER")
+    private String POLICY_NUMBER;
+
+    @Column(name = "POLICY_STATUS")
+    private String POLICY_STATUS;
+
+    @Column(name = "PRODUCT")
+    private String PRODUCT;
+
+    @Column(name = "REJECTION_REASON_CODE")
+    private String REJECTION_REASON_CODE;
+
+   
